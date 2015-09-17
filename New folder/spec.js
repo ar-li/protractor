8c9d0e444942d88c56ada7d7d8b1b085b4509ee2
ar-li-work <1>
2015-09-17 12:16:31 +0300

    new stuff
    to find links and click them

--- New folder/spec.js
+++ New folder/spec.js
@@ -1,19 +1,36 @@
 // spec.js
 describe('Protractor Demo App', function() {
-  var firstNumber = element(by.model('first'));
+	
+var AngularHomepage = function() {
+	this.get = function() {
+		browser.get('http://angular.github.io/angular-phonecat/step-12/app/#/phones');
+	};
+	this.findPhones = function() {
+		return element.all(by.repeater('phone in phones | filter:query | orderBy:orderProp'));
+	};
+	this.findPhone = function(num) {
+		return element.all(by.repeater('phone in phones | filter:query | orderBy:orderProp')).get(num).click();
+	};
+};
 
-  beforeEach(function() {
-    browser.get('http://angular.github.io/angular-phonecat/step-12/app/#/phones');
-  });
+	beforeEach(function() {
+		angularHomepage = new AngularHomepage();
+		angularHomepage.get();
+	});
 
-  it('gets all elements', function() {
-	  var el;
-	element.all(by.repeater('phone in phones | filter:query | orderBy:orderProp'))    .then(function(elements){
-	  return elements.length;
+	it('gets all elements', function() {
+		angularHomepage = new AngularHomepage();
+
+		angularHomepage.findPhones().then(function(elements){
+		return elements.length;
     })
     .then(function(count){
-      console.log("...." + count);
-      expect(count).toBeGreaterThan(0);
-  });
+		console.log("...." + count);
+		expect(count).toBeGreaterThan(0);
+	});
+	for(var i = 1; i<21;i++){
+	browser.findElement(by.xpath("//li[@class='thumbnail phone-listing ng-scope']["+i+"]/a[2]")).click();
+	angularHomepage.get();
+	};
 });
 });
