da6be8de462691e8de6d088277836b98d9632f52
ar-li-work <1>
2015-09-18 11:06:48 +0300

    new methods

--- New folder/spec.js
+++ New folder/spec.js
@@ -1,6 +1,6 @@
 // spec.js
 describe('Protractor Demo App', function() {
-	
+
 var AngularHomepage = function() {
 	this.get = function() {
 		browser.get('http://angular.github.io/angular-phonecat/step-12/app/#/phones');
@@ -16,25 +16,30 @@ var PhonePage = function() {
 	var pName;
 	this.getName = function() {
 		pName = browser.findElement(by.xpath("//h1[@class='ng-binding ng-scope']")).getText();
-		return pName
+		return pName;
 	};
 	this.printName = function() {
-		pname.then(function(text){
+        this.getName().then(function(text){
 			console.log(text);
 		});
 	};
-	this.imageSwitch = function() {
+	this.imgSwitch = function() {
+        browser.driver.findElements(by.xpath("//li[@class='ng-scope']")).then(function(elems){
+            for(i=1;i<elems.length;i++){
+                elems[i].click();
+            };
+        });
 
 	};
 	};
 
 	beforeEach(function() {
 		angularHomepage = new AngularHomepage();
+        pPage = new PhonePage();
 		angularHomepage.get();
 	});
 
 	it('gets all elements', function() {
-
 		angularHomepage.findPhones().then(function(elements){
 		return elements.length;
     })
@@ -43,11 +48,13 @@ var PhonePage = function() {
 		expect(count).toBeGreaterThan(0);
 	});
 	for(var i = 1; i<21;i++){
-	browser.findElement(by.xpath("//li[@class='thumbnail phone-listing ng-scope']["+i+"]/a[2]")).click();
-	browser.findElement(by.xpath("//h1[@class='ng-binding ng-scope']")).getText().then(function(text){
-		console.log(text);
-	});
-	angularHomepage.get();
+	    browser.findElement(by.xpath("//li[@class='thumbnail phone-listing ng-scope']["+i+"]/a[2]")).click();
+	    browser.findElement(by.xpath("//h1[@class='ng-binding ng-scope']")).getText().then(function(text){
+	    	console.log(text);
+	    });
+        pPage.printName();
+        pPage.imgSwitch();
+	    angularHomepage.get();
 	};
 	
 });
