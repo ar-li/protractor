fecd0b1e7382cf06bb67e53b66c73748250615a4
ar-li-work <1>
2015-09-23 11:39:41 +0300

    New stuff

--- New folder/spec.js
+++ New folder/spec.js
@@ -3,12 +3,13 @@
 var HomePage = require("./HomePage.js");
 var PhonePage = require("./PhonePage.js");
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 150000;
-describe('Protractor Demo App', function() {
+describe('Phone Page Testing start', function() {
 
 	beforeEach(function() {
 		angularHomepage = new HomePage();
         pPage = new PhonePage();
 		angularHomepage.get();
+
 	});
 
 	/*it('gets all elements', function() {
@@ -32,7 +33,7 @@ describe('Protractor Demo App', function() {
 		angularHomepage.searchPhone("XOOM");
 		expect(browser.getCurrentUrl()).toBe('http://angular.github.io/angular-phonecat/step-12/app/#/phones');
     });*/
-    it('Sorts alphabetically. Test-Sort-001', function(){
+    /*it('Sorts alphabetically. Test-Sort-001', function(){
 		//var bool = angularHomepage.sortM("name");
 		//expect(browser.getCurrentUrl()).toBe('http://angular.github.io/angular-phonecat/step-12/app/#/phones');
         //expect(bool).toBe(true);
@@ -40,12 +41,58 @@ describe('Protractor Demo App', function() {
       //  manSort = compSort.sort();
       //  expect(manSort).toEqual(compSort);
         a1 = angularHomepage.findPhones();
-        expect
-	});
+	});*/
 	/*
 	it('Opens a phone and checks if it\'s the chosen one. Test-Check-001', function(){
 		var href = angularHomepage.goToPhone(4);
 		expect(browser.getCurrentUrl()).toBe(href);
 	});*/
+    it('Checks if the search results are OK', function(){
+       /*old// doubleCheck = angularHomepage.findPhoneByOrder(1);
+        //angularHomepage.checkByName("4G");
+        angularHomepage.checkByName1("4G");
+            var doubleCheck;
+        doubleCheck = angularHomepage.checkByName1("4G");
+        expect(doubleCheck).toMatch("24G");
+
+       // expect(doubleCheck instanceof Object).toBe(true);
+
+        //expect(Array.isArray(doubleCheck)).toBe(true);
+       // expect(doubleCheck).toMatch("4G");
+
+        //expect(doubleCheck).toBeGreaterThan(0);*/
+		var elementsByHand;
+		var elementsBySearch;
+        var phoneCount;
+        var phoneText = [];
+		//elementsByHand = angularHomepage.checkMotor();
+		//elementsBySearch = angularHomepage.searchPhone("Motorola");
+        //elementsBySearch.getText().then(function(text){
+		//	console.log(text);
+		//});
+        /*angularHomepage.countPhones().then(function(num){
+        expect(num).toEqual(20);
+        for(var i = 1;i<=num;i++){
+            angularHomepage.findPhoneByOrder(i).getText().then(function(text){
+                //expect(text).toEqual("20");
+            })
+        };
+        });*/
+        angularHomepage.countPhones().then(function(num) {
+            for (var i = 1; i <= num; i++) {
+                angularHomepage.findPhoneByOrder(i).getText().then(function(text){
+                if (text.toLowerCase().indexOf("motorola") !== -1){
+                    phoneText.push(text);
+                    //console.log(phoneText[0]);
+                }
+                })
+            }
+        }).then(function(){
+        for (var i = 0; i<phoneText.length; i++){
+            console.log(phoneText);
+        }
+        });
+        //expect(angularHomepage.findPhoneByOrder(1)).toEqual(20);
+    });
 
 });
